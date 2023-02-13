correct evaluation score computation, included compute_score and test scripts

exp.py
@@ -47,23 +47,152 @@ def exists(tokens, sents):
     
     return False
 
-path = "./samanantar/newsamanantar_India_Delhi.txt"
-file = open(path)
-file_sents = file.readlines()
-cnt = 0
+def search(model, vec, w1, w2, w3):
+    sim = 0.0
+    index = ""
+
+    omitted = [w1, w2, w3]
+    words_dict = model.wv.key_to_index
+    words = [word for word in words_dict if word not in omitted]
+
+    for word in words:
+        v = model.wv[word]
+        score = np.matmul(np.array(vec), np.array(v))/(norm(np.array(vec))*norm(np.array(v)))
+        if score > sim: 
+            sim = score
+            index = word
+    
+    return index
+
+def evaluate(model, analogy_mappings, validation_mappings):
+    score = 0
+    cnt = 0
+
+    words_dict = model.wv.key_to_index
+
+    for key,value in analogy_mappings.items():
+        w1, w2 = key.split(":")[0], key.split(":")[1]
+        w3, w4 = value.split(":")[0], value.split(":")[1]
+
+        if w1 not in words_dict or w2 not in words_dict or w3 not in words_dict or w4 not in words_dict:
+            print("word not present")
+            continue
+
+        w1vec, w2vec = model.wv[w1], model.wv[w2]
+        w3vec = model.wv[w3]
+
+        # if cnt == 2:
+        #     print(w1vec)
+        
+        pred = w2vec - w1vec + w3vec
+        pred_word = search(model, pred, w1, w2, w3)
+
+        if w4 == pred_word: score += 1
+        cnt += 1
+
+        print("Actual: {}:{}::{}:{}, pred: {}".format(w1, w2, w3, w4, pred_word))
+    
+    acc = score/cnt
+    print("Accuracy is {}".format(acc))
+
+    cnt = 0
+    score = 0
+    
+    for key,value in validation_mappings.items():
+        w1, w2 = key.split(":")[0], key.split(":")[1]
+        w3, w4 = value.split(":")[0], value.split(":")[1]
+
+        if w1 not in words_dict or w2 not in words_dict or w3 not in words_dict or w4 not in words_dict:
+            print("word not present")
+            continue
+
+        w1vec, w2vec = model.wv[w1], model.wv[w2]
+        w3vec = model.wv[w3]
+
+        pred = w2vec - w1vec + w3vec
+        pred_word = search(model, pred, w1, w2, w3)
+
+        if w4 == pred_word: score += 1
+        cnt += 1
+
+        print("Actual: {}:{}::{}:{}, pred: {}".format(w1, w2, w3, w4, pred_word))
+    
+    acc = score/cnt
+    print("Accuracy is {}".format(acc))
+
+def index_word_maps(data: list) -> tuple:
+    
+    words = sorted(list(set(data)))
+    
+    word_to_index = {word: index for index, word in enumerate(words)}
+    index_to_word = {index: word for index, word in enumerate(words)}
+    return word_to_index, index_to_word
+
 sents = []
-for sent in file_sents:
-    tokens = sent.split()
-    tokens = preprocessing(tokens)
-    if len(tokens) > 20 and len(tokens) < 6: continue
-    if not exists(tokens, sents):
-        sents.append(tokens)
-    cnt += 1
-    #if cnt > 120: break
+for f in os.listdir("./data/"):
+    path = "./data/{}".format(f)
+    file = open(path)
+    file_sents = file.readlines()
+    
+    cnt = 0
+    for sent in file_sents:
+        tokens = sent.split()
+        tokens = preprocessing(tokens)
+        if len(tokens) > 25 and len(tokens) < 6: continue
+        if tokens not in sents:
+            sents.append(tokens)
+            cnt += 1
+        if cnt > 500: break
+
+f = open("./gutenberg.txt")
+lines = f.readlines()
+for sent in lines:
+    sents.append(sent.replace("\n", "").split())
+f.close()
 random.shuffle(sents)
 
-model = Word2Vec(sentences=sents, vector_size=300, window=5, min_count=1, workers=4, sg=0, cbow_mean=1)
-sims = model.wv.most_similar('india', topn=10)
-print(sims)
+# collect all the words
+words = []
+for sent in sents:
+    words += sent
+
+f = open("Analogy_dataset.txt")
+lines = f.readlines()
+analogy_mappings = {}
+analogy_words = {}
+for line in lines:
+    words_ = line.split()
+    words_ = [word.lower() for word in words_]
+    analogy_mappings[words_[0]+":"+words_[1]] = words_[2]+":"+words_[3]
+    analogy_words[words_[0]] = 1
+    analogy_words[words_[1]] = 1
+    analogy_words[words_[2]] = 1
+    analogy_words[words_[3]] = 1
+f.close()
+
+f = open("Validation.txt")
+lines = f.readlines()
+validation_mappings = {}
+validation_words = {}
+for line in lines:
+    words_ = line.split()
+    words_ = [word.lower() for word in words_]
+    validation_mappings[words_[0]+":"+words_[1]] = words_[2]+":"+words_[3]
+    validation_words[words_[0]] = 1
+    validation_words[words_[1]] = 1
+    validation_words[words_[2]] = 1
+    validation_words[words_[3]] = 1
+f.close()
+
+analogy_words = list(analogy_words.keys())
+validation_words = list(validation_words.keys())
+
+# add analogy and validation words to words list
+for word in analogy_words: words.append(word.lower())
+for word in validation_words: words.append(word.lower())
+
+model = Word2Vec(sentences=sents, vector_size=150, window=5, min_count=1, workers=4, sg=0, cbow_mean=1, epochs=20)
+
+evaluate(model, analogy_mappings, validation_mappings)
 # sims = model.wv.most_similar('kathmandu', topn=10)
 # print(sims)

test.py
@@ -7,9 +7,8 @@ import pickle
 from numpy.linalg import norm
 import random
 from tqdm import tqdm
+import argparse
 
-batch_size = 200
-num_epochs = 3
 
 def softmax(z):
     e_z = np.exp(z)
@@ -19,11 +18,11 @@ def softmax(z):
 class word2vec(object):
 
     def __init__(self):
-        self.N = 300
+        self.N = 100
         self.X_train = []
         self.y_train = []
-        self.window_size = 3
-        self.alpha = 0.08
+        self.window_size = 4
+        self.alpha = 0.09
         self.words = []
         self.word_index = {}
         self.validation_mappings = {}
@@ -32,8 +31,9 @@ class word2vec(object):
 
     def initialize(self,V, data, word_index, validation_mappings, analogy_mappings, index_word):
         self.V = V
-        self.W1 = np.random.uniform(-1, 1, (self.V, self.N))
-        self.W2 = np.random.uniform(-1, 1, (self.N, self.V))
+        np.random.seed(1)
+        self.W1 = np.random.rand(self.V, self.N)
+        self.W2 = np.random.rand(self.N, self.V)
 
         self.words = data
         for i in range(len(data)):
@@ -51,8 +51,8 @@ class word2vec(object):
         
         assert X_.shape == (self.V, len(X))
         
-        self.h = np.dot(self.W1.T, X_)
-        self.u = np.dot(self.W2.T, self.h)
+        self.h = np.matmul(self.W1.T, X_)
+        self.u = np.matmul(self.W2.T, self.h)
         self.y_hat = softmax(self.u)
         
         assert self.y_hat.shape == (self.V, len(X))
@@ -67,8 +67,8 @@ class word2vec(object):
         assert x_.shape == (self.V, len(x))
 
         e = self.y_hat - t_
-        self.grad_W2 = np.dot(self.h, e.T)
-        self.grad_W1 = np.dot(x_, np.dot(self.W2, e).T)
+        self.grad_W2 = np.matmul(self.h, e.T)
+        self.grad_W1 = np.matmul(x_, np.matmul(self.W2, e).T)
         self.W1 = self.W1 - self.alpha * self.grad_W1
         self.W2 = self.W2 - self.alpha * self.grad_W2
     
@@ -87,213 +87,89 @@ class word2vec(object):
 
         return loss
 
+def isEnglish(word):
+    hasNum = any(chr.isdigit() for chr in word)
+    flag = False
+    for char in word:
+        if (char >= 'a' and char <= 'z') or (char >= 'A' and char <= 'Z'):
+            continue
+        flag = True
+        break
 
-def train_model(w2v, fields, word_to_index, num_epochs):
-    loss = 0.0
-    step = 0
-
-    batch_x = []
-    batch_y = []
-    for epoch in range(num_epochs):
-        
-        for art_num, article in enumerate(fields):
-            training_data = gutenberg.sents(article)
-            num_sents = len(training_data)
-            print(num_sents)
-
-            for m in range(0,len(training_data)):
-                sent = training_data[m]
-                sent = preprocessing(sent)
-
-                for i in range(0,len(sent)):
-                    context_words = []
-                    train_word = word_to_one_hot_vector(sent[i],word_to_index,len(word_to_index.keys()))
-                    
-                    for j in range(i-w2v.window_size,i+w2v.window_size):
-                            if i!=j and j>=0 and j<len(sent):
-                                context_words.append(sent[j])
-                    
-                    if len(context_words) > 0:
-                        y = train_word
-                        x = context_words_to_vector(context_words,word_to_index)
-                        batch_y.append(y)
-                        batch_x.append(x)
-                    
-                    if len(batch_x) == batch_size:
-                        loss = w2v.train(batch_x, batch_y)
-                        step += 1
-                        batch_x = []
-                        batch_y = []
-                        if step % 20 == 0: print("step: {}, loss: {}, epoch: {}".format(step, loss, epoch))
-                        if step % 100 == 0:
-                            evaluate(w2v)
-    
-    if len(batch_x) > 0:
-        loss = w2v.train(batch_x, batch_y)
-        print("step: {}, loss: {}".format(step+1, loss))
-
+    return not (hasNum or flag)
 
 def preprocessing(sentence):
-    stop_words = set(stopwords.words('english'))   
-    x = [word.strip(string.punctuation) for word in sentence
-                                     if word not in stop_words]
-    x = [word for word in x if len(word) != 0]
-    x = [word.lower() for word in x]
+    stop_words = set(stopwords.words('english'))
+    
+    # remove stopwords and punctuation
+    x = [word.strip(string.punctuation).lower() for word in sentence]
+    x = [word for word in x if word not in stop_words]
+
+    # remove words containing numbers and non-english characters
+    x = [word for word in x if isEnglish(word)]
+    x = [word for word in x if len(word) != 0 and len(word) > 2]
     
     return x
 
+def topSimilar(w2v, pred, k, w1, w2, w3):
 
-def index_word_maps(data: list) -> tuple:
+    v1 = pred
+    omit = [w1, w2, w3]
+    indices = [val for key,val in w2v.word_index.items() if key not in omit]
     
-    words = sorted(list(set(data)))
+    similarities = []
+    for index in indices:
+        vec = w2v.W1[index]
+        similarities.append((index,np.matmul(np.array(vec), np.array(v1))/(norm(np.array(vec))*norm(np.array(v1)))))
+
+    similarities.sort(reverse=True, key = lambda x: x[1])
     
-    word_to_index = {word: index for index, word in enumerate(words)}
-    index_to_word = {index: word for index, word in enumerate(words)}
-    return word_to_index, index_to_word
-
-
-def word_to_one_hot_vector(word: str, word_to_index: dict, vocabulary_size: int) -> np.ndarray:
-    one_hot_vector = np.zeros(vocabulary_size)
-    one_hot_vector[word_to_index[word]] = 1
-    return one_hot_vector
-
-
-def context_words_to_vector(context_words: list,
-                            word_to_index: dict) -> np.ndarray:
-    vocabulary_size = len(word_to_index)
-    context_words_vectors = [
-        word_to_one_hot_vector(word, word_to_index, vocabulary_size)
-        for word in context_words]
-    return np.mean(context_words_vectors, axis=0)
-
-
-
-def search(w2v, vec, w1, w2, w3):
-    sim = 0.0
-    index = 0
-
-    omitted = [w1, w2, w3]
-    indices = [val for val in w2v.word_index.values() if val not in omitted]
-
-    for word in indices:
-        v = w2v.W1[word]
-        score = np.dot(np.array(vec), np.array(v))/(norm(np.array(vec))*norm(np.array(v)))
-        if score > sim: 
-            sim = score
-            index = word
-    
-    return index
-
-
-def evaluate(w2v):
-    score = 0
-    cnt = 0
-    
-    for key,value in w2v.analogy_mappings.items():
-        w1, w2 = key.split(":")[0], key.split(":")[1]
-        w3, w4 = value.split(":")[0], value.split(":")[1]
-
-        if w1 not in w2v.word_index or w2 not in w2v.word_index or w3 not in w2v.word_index or w4 not in w2v.word_index:
-            print("word not present")
-            continue
-
-        w1vec, w2vec = w2v.W1[w2v.word_index[w1]], w2v.W1[w2v.word_index[w2]]
-        w3vec = w2v.W1[w2v.word_index[w3]]
-
-        # if cnt == 2:
-        #     print(w1vec)
-        
-        pred = w2vec - w1vec + w3vec
-        index = search(w2v, pred, w2v.word_index[w1], w2v.word_index[w2], w2v.word_index[w3])
-        actual = w2v.word_index[w4]
-
-        if actual == index: score += 1
-        cnt += 1
-
-        print("Actual: {}:{}::{}:{}, pred: {}".format(w1, w2, w3, w4, w2v.index_word[index]))
-    
-    acc = score/cnt
-    print("Accuracy is {}".format(acc))
-    print()
-
-    score = 0
-    cnt = 0
-    for key,value in w2v.validation_mappings.items():
-        w1, w2 = key.split(":")[0], key.split(":")[1]
-        w3, w4 = value.split(":")[0], value.split(":")[1]
-
-        if w1 not in w2v.word_index or w2 not in w2v.word_index or w3 not in w2v.word_index or w4 not in w2v.word_index:
-            print("word not present")
-            continue
-
-        w1vec, w2vec = w2v.W1[w2v.word_index[w1]], w2v.W1[w2v.word_index[w2]]
-        w3vec = w2v.W1[w2v.word_index[w3]]
-
-        # if cnt == 2:
-        #     print(w1vec)
-        
-        pred = w2vec - w1vec + w3vec
-        index = search(w2v, pred, w2v.word_index[w1], w2v.word_index[w2], w2v.word_index[w3])
-        actual = w2v.word_index[w4]
-
-        if actual == index: score += 1
-        cnt += 1
-
-        print("Actual: {}:{}::{}:{}, pred: {}".format(w1, w2, w3, w4, w2v.index_word[index]))
-    
-    acc = score/cnt
-    print("Accuracy is {}".format(acc))
-
+    for i in range(k):
+        print(similarities[i][1], w2v.index_word[similarities[i][0]])
 
 def main():
 
-    f = open("Analogy_dataset.txt")
-    lines = f.readlines()
-    analogy_mappings = {}
-    analogy_words = {}
-    for line in lines:
-        words_ = line.split()
-        words_ = [word.lower() for word in words_]
-        analogy_mappings[words_[0]+":"+words_[1]] = words_[2]+":"+words_[3]
-        analogy_words[words_[0]] = 1
-        analogy_words[words_[1]] = 1
-        analogy_words[words_[2]] = 1
-        analogy_words[words_[3]] = 1
-    f.close()
-
-    f = open("Validation.txt")
-    lines = f.readlines()
-    validation_mappings = {}
-    validation_words = {}
-    for line in lines:
-        words_ = line.split()
-        words_ = [word.lower() for word in words_]
-        validation_mappings[words_[0]+":"+words_[1]] = words_[2]+":"+words_[3]
-        validation_words[words_[0]] = 1
-        validation_words[words_[1]] = 1
-        validation_words[words_[2]] = 1
-        validation_words[words_[3]] = 1
-    f.close()
-
-    analogy_words = list(analogy_words.keys())
-    validation_words = list(validation_words.keys())
-
-    words = []
-    # add analogy and validation words to words list
-    for word in analogy_words: words.append(word)
-    for word in validation_words: words.append(word)
-    
-    # lower case all words
-    words = [word.lower() for word in words]
-    word_to_index, index_to_word = index_word_maps(words)
-
-    
-    # save weights
-    with open('w2vecbow.pkl', 'rb') as f:
+    # load weights
+    with open('w2vecbow_v4.pkl', 'rb') as f:
         w2v = pickle.load(f)
+
+    # collect the input sentence
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-i1", "--Input1", help = "Provide Input")
+    parser.add_argument("-i2", "--Input2", help = "Provide Input")
+    parser.add_argument("-i3", "--Input3", help = "Provide Input")
+    parser.add_argument("-k", "--k", help = "Provide Input")
+
+    args = parser.parse_args()
+
+    w1 = args.Input1
+    w2 = args.Input2
+    w3 = args.Input3
+    k = int(args.k)
+
+    # pre-process words
+    words = preprocessing([w1, w2, w3])
+    w1, w2, w3 = words[0], words[1], words[2]
+
+    if w1 not in w2v.word_index:
+        print("{} not present in vocabulary".format(w1))
+        exit() 
+    if w2 not in w2v.word_index:
+        print("{} not present in vocabulary".format(w2))
+        exit()
+    if w3 not in w2v.word_index:
+        print("{} not present in vocabulary".format(w3))
+        exit()
+    
+    w1vec, w2vec = w2v.W1[w2v.word_index[w1]], w2v.W1[w2v.word_index[w2]]
+    w3vec = w2v.W1[w2v.word_index[w3]]
+    
+    pred = w2vec - w1vec + w3vec
+
+    topSimilar(w2v, pred, k, w1, w2, w3)
     
 
-    evaluate(w2v)
 
 
 if __name__ == "__main__":